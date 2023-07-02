112e4dad5dd4febecff930f35610b14e6bf12f20
Anthony Tan <atan5133@gmail.com>
2023-07-01 22:57:29 -0700

    Add comparison between std dev and standard error

--- sample_distribution.R
+++ sample_distribution.R
@@ -4,7 +4,7 @@ data(ChickWeight)
 weights <- ChickWeight$weight
 
 n <- 50
-num_experiments <- 500
+num_experiments <- 10000
 
 experiment_results <- replicate(
     num_experiments, sample(weights, n, replace = TRUE)
@@ -20,4 +20,22 @@ hist(
     col = "lightblue",
     freq = FALSE,
     ylab = "Probability Density"
+)
+
+# When you run the experiment a lot of times
+# you can create a normal sample distribution
+# std_dev of the means will be similar to SE
+std_dev_of_means <- sd(means)
+
+one_experiment <- experiment_results[, 1]
+std_dev_of_samples <- sd(one_experiment)
+std_error <- std_dev_of_samples / sqrt(length(one_experiment))
+
+sprintf(
+    paste(
+        "Std_dev of Means: %f", "Std_error: %f",
+        sep = ". "
+    ),
+    std_dev_of_means,
+    std_error
 )
